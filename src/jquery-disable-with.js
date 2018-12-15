0f0ad320a3534203fac9ffb489465fb6626ffdea
Anduin2017 <anduin@aiursoft.com>
2018-12-15 23:26:15 +0800

    Support jquery validate.

--- src/jquery-disable-with.js
+++ src/jquery-disable-with.js
@@ -5,6 +5,12 @@ $(document).ready(function () {
         var submitButton = $(this);
         var isButton = submitButton.is('button');
         var value = submitButton.attr('data-disable-with');
+        var prevalue = '';
+        if (isButton) {
+            prevalue = submitButton.html();
+        } else {
+            prevalue = submitButton.val();
+        }
         var firstForm = submitButton.parents().filter("form").first();
         firstForm.on('submit', function () {
             //Set
@@ -13,6 +19,14 @@ $(document).ready(function () {
                 submitButton.html(value);
             } else {
                 submitButton.val(value);
+            }
+        });
+        submitButton.parents().filter("form").first().bind('invalid-form.validate', function () {
+            submitButton.removeAttr('disabled');
+            if (submitButton.is('button')) {
+                submitButton.html(prevalue);
+            } else {
+                submitButton.val(prevalue);
             }
         });
     });
