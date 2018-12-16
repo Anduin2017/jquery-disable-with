a44ba3646cb2a53edd208bd333acc38bb959ac8b
Anduin2017 <anduin@aiursoft.com>
2018-12-16 10:59:58 +0800

    Better bundle experience.

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,12 @@ var concat = require('gulp-concat');
 var expect = require('gulp-expect-file');
 var inputFiles = 'src/jquery-disable-with.js';
 
-gulp.task("bundle", function () {
+gulp.task("bundle", function (done) {
     gulp.src(inputFiles)
         .pipe(expect(inputFiles))
         .pipe(concat('temp'))
         .pipe(uglify())
         .pipe(rename('jquery-disable-with.min.js'))
         .pipe(gulp.dest('dist'));
+    done();
 });
